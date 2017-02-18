ba0fecf7d2fa0971624e0aa22a9727318331f94d
袁其亮 <yuanqiliang@51kik.com>
2017-02-18 11:28:57 +0800

    init

---
+++ src/main/java/com/lance/algorithm/App.java
@@ -0,0 +1,10 @@
+package com.lance.algorithm;
+
+/**
+ * Created by perdonare on 2017/2/18.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
